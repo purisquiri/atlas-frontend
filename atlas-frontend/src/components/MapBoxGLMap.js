cf950085fba3a817eca46a5d98663bf0ade164c7
JakesProjects1 <jacob@kagon.net>
2020-12-24 12:04:46 -0800

    map is up and component files built

---
+++ atlas-frontend/src/components/MapBoxGLMap.js
@@ -0,0 +1,71 @@
+import '../Map.css'
+import mapboxgl from "mapbox-gl";
+import React, { useState, useEffect } from "react";
+
+const MapboxGLMap = () => {
+    // const [map] = useState(null);
+    const [countries, updateCountries] = useState(["in", "iso_3166_1_alpha_3"]);
+    const [search, updateSearch] = useState("")
+    const mapContainer = React.createRef();
+  
+    useEffect(() => {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiamFjb2JrYWdvbiIsImEiOiJja2owZHM4NGUxbTN1MnJtd25pbHh5a2YwIn0.Ipv-6ntDnJw4WUD9ly3gcw";
+      
+        const map = new mapboxgl.Map({
+         container: "map",
+          style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+          center: [5, 46],
+          zoom: 2,
+        });
+  
+        map.on("load", () => {
+          // setMap(map);
+          // map.resize();
+          map.addLayer(
+            {
+              id: "country-boundaries",
+              source: {
+                type: "vector",
+                url: "mapbox://mapbox.country-boundaries-v1",
+              },
+              "source-layer": "country_boundaries",
+              type: "fill",
+              paint: {
+                "fill-color": "#d2361e",
+                "fill-opacity": 0.4,
+              },
+            },
+            "country-label"
+          );
+          map.setFilter("country-boundaries", countries);
+        });
+  
+      
+    });
+  
+    const handleSearch = (event) => {
+      //   updateSearch(search)
+      console.log(event.target.value)
+        
+    }
+  
+  
+     const handleChange = (event) => {
+     updateCountries([...countries, search])
+  
+      console.log(countries)
+      console.log(event.target.value.toString())
+      
+    };
+  
+    return (
+    <div>
+    {
+    <div id="map"></div>
+    }
+  </div>
+    )};
+  
+  export default MapboxGLMap;
+  
---
+++ atlas-frontend/src/containers/HomeContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import MapboxGLMap from '../components/MapBoxGLMap'
+
+class HomeContainer extends Component {
+    render() {
+        return (
+            <div>
+                <MapboxGLMap/>
+            </div>
+        );
+    }
+}
+
+export default HomeContainer;
+
+
+
