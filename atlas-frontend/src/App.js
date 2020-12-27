dea78fc053124273a8e297d53091a8a7f851df40
JakesProjects1 <jacob@kagon.net>
2020-12-27 14:41:28 -0800

    started on routes

--- atlas-frontend/src/App.js
+++ atlas-frontend/src/App.js
@@ -3,13 +3,14 @@ import LoginContainer from './containers/LoginContainer'
 import HomeContainer from './containers/HomeContainer'
 import Navbar from './containers/Navbar' 
 import SignIn from './containers/LoginContainer';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         {/* <SignIn/> */}
-        <HomeContainer />
+        < HomeContainer />
         <Navbar/>
         
 
