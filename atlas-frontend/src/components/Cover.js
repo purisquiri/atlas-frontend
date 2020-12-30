74326ee64a19e5a26a126cb402a35c057f60589b
purisquiri <mau.dorigo@outlook.es>
2020-12-30 16:04:55 -0500

    cover and globe

---
+++ atlas-frontend/src/components/Cover.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Cover() {
+  return (
+    <div>
+      <div className="site-wrapper">
+        <div className="site-wrapper-inner">
+          <div className="container">
+            <div className="masthead clearfix">
+              <div className="container inner">
+                <h3 className="masthead-brand"></h3>
+                <nav>
+                  <ul className="nav masthead-nav">
+                    <li className="active">
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">Map</a>
+                    </li>
+                    <li>
+                      <a href="#">SignUp</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
